da76f7b9ca979644ef595a9e6e797efcf94b0c59
Sarath Menon <sarathmenon@mailbox.org>
2022-06-24 12:06:39 +0200

    update tests

--- tests/test_read_files.py
+++ tests/test_read_files.py
@@ -9,3 +9,10 @@ def test_file_rrng():
 def test_file_pos():
     datapos = read_pos("tests/data/R31_06365-v02.pos")
     assert np.isclose(datapos[0][0]+5.3784895, 0)
+
+def test_file_df():
+    data = read_apt_to_df("tests/data")
+    assert np.isclose(data[0][0]["x"].values[0]+5.3784895, 0)
+    assert data[1][0] == 'R31_06365-v02.pos'
+    assert data[2]["name"].values[0] == "C"
+    assert np.isclose(data[3]["lower"].values[0]-5.974, 0)
