a3787a575fa6f46df296f038fb67a9ce8ec130eb
Sarath Menon <sarathmenon@mailbox.org>
2022-06-24 11:26:32 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
     #entry_points={
     #    'console_scripts': [
