5514f1ffbe5a4fd7fe579a7c1d602a4415e4198f
Sarath Menon <sarathmenon@mailbox.org>
2022-06-28 13:34:51 +0200

    restructure methods

--- compositionspace/comp_analysis_cluster.py
+++ compositionspace/comp_analysis_cluster.py
@@ -10,6 +10,8 @@ import time
 #import paraprobe_transcoder
 import h5py
 import warnings
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
 
 #really check this!
 pd.options.mode.chained_assignment = None
@@ -344,101 +346,105 @@ def calculate_voxel_composition(small_chunk_file_name, outfilename="3Vox_ratios_
     
     
     
-def centeroid_df(data_frame):
-    length = len(data_frame['x'])
-    sum_x = np.sum(data_frame['x'])
-    sum_y = np.sum(data_frame['y'])
-    sum_z = np.sum(data_frame['z'])
-    return sum_x/length, sum_y/length, sum_z/length
+def calculate_centroid(data):
+    """
+    Calculate centroid
 
-def centeroid_np_array(array):
-    length = len(array[:,0])
-    sum_x = np.sum(array[:,0])
-    sum_y = np.sum(array[:,1])
-    sum_z = np.sum(array[:,2])
-    return sum_x/length, sum_y/length, sum_z/length
+    Parameters
+    ----------
+    data: pandas DataFrame or numpy array
+
+    Returns
+    -------
+    centroid
+    """
+    if isinstance(data, pd.DataFrame):
+        length = len(data['x'])
+        sum_x = np.sum(data['x'])
+        sum_y = np.sum(data['y'])
+        sum_z = np.sum(data['z'])
+        return sum_x/length, sum_y/length, sum_z/length
+    else:
+        length = len(data[:,0])
+        sum_x = np.sum(data[:,0])
+        sum_y = np.sum(data[:,1])
+        sum_z = np.sum(data[:,2])
+        return sum_x/length, sum_y/length, sum_z/length
 
 
-def PCA_cumsum(VoxRatioFile, VoxFile ):
-    from sklearn.decomposition import PCA
+def calculate_PCA_cumsum(vox_ratio_file, vox_file):
 
-    with h5py.File(VoxFile,"r") as hdf:
+    with h5py.File(vox_file,"r") as hdf:
         group = hdf.get("Group_sm_vox_xyz_Da_spec")
         group0 = hdf.get("0")
-        spec_lst = list(list(group0 .attrs.values())[2])
-    #print(spec_lst)
+        spec_lst = list(list(group0.attrs.values())[2])
     
-    with h5py.File(VoxRatioFile , "r") as hdfr:
-        Ratios = np.array(hdfr.get("vox_ratios"))
-        Ratios_colomns = list(list(hdfr.attrs.values())[0])
-        Group_name = list(list(hdfr.attrs.values())[1])
+    with h5py.File(vox_ratio_file , "r") as hdfr:
+        ratios = np.array(hdfr.get("vox_ratios"))
+        ratios_columns = list(list(hdfr.attrs.values())[0])
+        group_name = list(list(hdfr.attrs.values())[1])
         
-    Ratios =pd.DataFrame(data=Ratios, columns=Ratios_colomns)   
+    ratios = pd.DataFrame(data=ratios, columns=ratios_columns)   
 
-    X_train=Ratios.drop(['Total_no','vox'], axis=1)
+    X_train = ratios.drop(['Total_no', 'vox'], axis=1)
+    
     PCAObj = PCA(n_components = len(spec_lst)) 
     PCATrans = PCAObj.fit_transform(X_train)
-    PCAObj.explained_variance_
     PCACumsumArr = np.cumsum(PCAObj.explained_variance_ratio_)
-    return PCACumsumArr, Ratios
+    
+    return PCACumsumArr, ratios
 
-def NumClusters(VoxRatioFile, VoxFile, NumClustersVal ):
-    from sklearn.mixture import GaussianMixture
+def find_num_clusters(vox_ratio_file, vox_file, num_clusters):
+    
 
-    PCACumsumArr, Ratios = PCA_cumsum(VoxRatioFile, VoxFile )
-    #import tqdm
+    PCACumsumArr, Ratios = calculate_PCA_cumsum(vox_ratio_file, vox_file)
+    
     gm_scores=[]
     aics=[]
     bics=[]
     X = Ratios.drop(['Total_no','vox'], axis=1)
-    n_clusters=list(range(1,NumClustersVal))
+    n_clusters=list(range(1, num_clusters))
+    
     for n_cluster in tqdm(n_clusters):
-        gm = GaussianMixture(n_components=n_cluster,verbose=0)
+        gm = GaussianMixture(n_components=n_cluster, verbose=0)
         gm.fit(X)
         y_pred=gm.predict(X)
-        #gm_scores.append(homogeneity_score(y,y_pred))
         aics.append(gm.aic(X))
         bics.append(gm.bic(X))
         
-    return NumClustersVal, aics, bics
+    return aics, bics
 
 
-def VoxRead(VoxFile, FilesArr):
+def get_centroid_from_vox(vox_file, files_arr):
 
-    with h5py.File(VoxFile,"r") as hdf:
-        #group = hdf.get("Group_sm_vox_xyz_Da_spec")
-        #spec_lst = list(list(group.attrs.values())[1])
+    with h5py.File(vox_file, "r") as hdf:
         items = list(hdf.items())
         item_lst = []
         for item in range(len(items)):
             item_lst.append([100000*(item),100000*(item+1)])
         item_lst = np.array(item_lst)
 
-
-        Dic_centroids = {}
-        Dic_centroids["x"]=[]
-        Dic_centroids["y"]=[]
-        Dic_centroids["z"] = []
-        Dic_centroids["file_name"] = []
-        Df_centroids = pd.DataFrame(columns=['x', 'y', 'z','filename'])
-        for filename in tqdm(FilesArr):
-            #file_name_grp = "Group_sm_vox_xyz_Da_spec/"+"{}".format(int(i))
+        dic_centroids = {}
+        dic_centroids["x"]=[]
+        dic_centroids["y"]=[]
+        dic_centroids["z"] = []
+        dic_centroids["file_name"] = []
+        df_centroids = pd.DataFrame(columns=['x', 'y', 'z','filename'])
+        
+        for filename in tqdm(files_arr):
             group = np.min(item_lst[[filename in range(j[0],j[1]) for j in item_lst]])
-            xyz_Da_spec_atoms = np.array(hdf.get("{}/{}".format(group,filename)))
-            x,y,z = centeroid_np_array(xyz_Da_spec_atoms)
-            Dic_centroids["x"].append(x)
-            Dic_centroids["y"].append(y)
-            Dic_centroids["z"].append(z)
-            Dic_centroids["file_name"].append(filename)
-            #Df_centroids = Df_centroids.append({'x': x, 'y': y, 'z': z, 'filename' : i}, ignore_index=True)
-            
-    return Dic_centroids
+            xyz_Da_spec_atoms = np.array(hdf.get("{}/{}".format(group, filename)))
+            x, y, z = calculate_centeroid(xyz_Da_spec_atoms)
+            dic_centroids["x"].append(x)
+            dic_centroids["y"].append(y)
+            dic_centroids["z"].append(z)
+            dic_centroids["file_name"].append(filename)            
+    return dic_centroids
 
 
-def CompositionCluster(VoxRatioFile, VoxFile, n_components):
-    from sklearn.mixture import GaussianMixture
+def find_cluster_composition(vox_ratio_file, vox_file, n_components, save=False, outfile=None):
 
-    PCACumsumArr, Ratios = PCA_cumsum(VoxRatioFile, VoxFile )
+    PCACumsumArr, Ratios = calculate_PCA_cumsum(vox_ratio_file, vox_file)
     X = Ratios.drop(['Total_no','vox'], axis=1)
     
     gm = GaussianMixture(n_components=3, max_iter=100000,verbose=0)
@@ -452,17 +458,17 @@ def CompositionCluster(VoxRatioFile, VoxFile, n_components):
     df_dis2 = Ratios.iloc[cluster_2.flatten()]
     df_dis1 = Ratios.iloc[cluster_1.flatten()]
     df_dis0 = Ratios.iloc[cluster_0.flatten()]
-    return df_dis2, df_dis1,df_dis0, cluster_0,cluster_1,cluster_2,Ratios
+
+    if save:
+        save_composition_clusters(df_dis2, df_dis1, df_dis0, cluster_0, cluster_1, cluster_2, Ratios, outfile, vox_file)
+
+    return df_dis2, df_dis1,df_dis0, cluster_0,cluster_1,cluster_2, Ratios
     
-def SaveCompositionClusters(VoxRatioFile, VoxFile, OutFile,n_components):
-    df_dis2, df_dis1,df_dis0, cluster_0,cluster_1,cluster_2,Ratios = CompositionCluster(VoxRatioFile, VoxFile, n_components)
+def save_composition_clusters(df_dis2, df_dis1, df_dis0, cluster_0, cluster_1, cluster_2, Ratios, outfile, vox_file):
     
     plot_files_cl_0 = []
     plot_files_cl_1 = []
     plot_files_cl_2 = []
-
-    #for i in cluster_2.flatten():
-    #    plot_files_cl_2.append(Ratios['filename'][i])
 
     for i in cluster_0.flatten():
         plot_files_cl_0.append(Ratios['vox'][i])
@@ -472,52 +478,45 @@ def SaveCompositionClusters(VoxRatioFile, VoxFile, OutFile,n_components):
     for i in cluster_2.flatten():
         plot_files_cl_2.append(Ratios['vox'][i])
         
-        
-    #get the item name for h5 file
     plot_files_cl_0_group = [int(file_num) for file_num in plot_files_cl_0 ]
     plot_files_cl_1_group = [int(file_num) for file_num in plot_files_cl_1 ]
     plot_files_cl_2_group = [int(file_num) for file_num in plot_files_cl_2 ]
     
-    hdfw = h5py.File(OutFile,"w")
+    hdfw = h5py.File(outfile,"w")
 
     G1 = hdfw.create_group("All")
     G1.attrs["what"] = ["Centroid of All voxels"]
     G1.attrs["howto_Group_name"] = ["Group_sm_vox_xyz_Da_spec/"]
-    G1.attrs["colomns"] = ["x","y","z","file_name"]
+    G1.attrs["columns"] = ["x","y","z","file_name"]
 
     G2 = hdfw.create_group("Phase1")
     G2.attrs["what"] = ["Centroid of Phase1 voxels"]
     G2.attrs["howto_Group_name"] = ["Group_sm_vox_xyz_Da_spec/"]
-    G2.attrs["colomns"] = ["x","y","z","file_name"]
+    G2.attrs["columns"] = ["x","y","z","file_name"]
 
     G3 = hdfw.create_group("Phase2")
     G3.attrs["what"] = ["Centroid of Phase2 voxels"]
     G3.attrs["howto_Group_name"] = ["Group_sm_vox_xyz_Da_spec/"]
-    G3.attrs["colomns"] = ["x","y","z","file_name"]
+    G3.attrs["columns"] = ["x","y","z","file_name"]
     
-    
-    ##All
-    import os
-    #Small_chunk_file_name = "./file_file_D3_High_Hc_R5076_53143_apt_large_chunks_test_arr_h5_small_chunks_test_arr4.h5"
-    hdf_sm_r = h5py.File(VoxFile,"r")
+    hdf_sm_r = h5py.File(vox_file, "r")
     group = hdf_sm_r.get("0")
-    Total_Voxels =list(list(group.attrs.values())[0])
+    total_voxels =list(list(group.attrs.values())[2])
 
-    Total_Voxels_int =""
-    for number in Total_Voxels:
-        Total_Voxels_int = Total_Voxels_int+ number
+    total_voxels_int =""
+    for number in total_voxels:
+        total_voxels_int = total_voxels_int + number
 
-    Total_Voxels_int = int(Total_Voxels_int)
+    total_voxels_int = int(total_voxels_int)
     hdf_sm_r.close()
-    plot_files_cl_All_group = [file_num for file_num in range(Total_Voxels_int)]
-    #files = [file_num for file_num in range(905667)]
+    plot_files_cl_All_group = [file_num for file_num in range(total_voxels_int)]
 
-    FilesList = [plot_files_cl_All_group, plot_files_cl_1_group, plot_files_cl_2_group]
-    GroupsList = [G1,G2,G3]
-    PhasesNames = ["All","Ph1", "Ph2"] 
+    files_list = [plot_files_cl_All_group, plot_files_cl_1_group, plot_files_cl_2_group]
+    groups_list = [G1, G2, G3]
+    phases_names = ["All","Ph1", "Ph2"] 
     
-    for i in range(len(FilesList)):
+    for i in range(len(files_list)):
         
-        CentroidsDic = VoxRead(VoxFile, FilesArr= FilesList[i])
-        GroupsList[i].create_dataset("{}".format(PhasesNames[i]), data = pd.DataFrame.from_dict(CentroidsDic).values)
+        centroids_cic = get_centroid_from_vox(vox_file, files_arr = files_list[i])
+        groups_list[i].create_dataset("{}".format(phases_names[i]), data = pd.DataFrame.from_dict(centroids_dic).values)
     hdfw.close()
